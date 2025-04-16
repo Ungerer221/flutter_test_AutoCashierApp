534de38348030c55ac3bdd717868527f93af781f
Ungerer221 <221302@virtualwindow.co.za>
2025-04-16 19:16:28 +0200

    Create simple_calc_state.dart

---
+++ cashierapp/lib/cubits/simple_calc_state.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+// creating the state (data structure)
+
+class SimpleCalcState extends Equatable {
+  final double productPrice;
+  final double amountPaid;
+  final Map<String, int> changeBreakdown;
+
+  
+}
