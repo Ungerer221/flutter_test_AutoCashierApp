9ef76057cefca1a0a93cee7f1891ae29f8441714
Ungerer221 <221302@virtualwindow.co.za>
2025-04-16 21:32:48 +0200

    Update main.dart

--- cashierapp/lib/main.dart
+++ cashierapp/lib/main.dart
@@ -9,10 +9,61 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      //root widget
       home: Scaffold(
+        appBar: AppBar(title: const Text('My new Cashier App')),
         body: Center(
-          child: Text('Hello World!'),
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  const Text("Auto Cashier at your service"),
+                  const SizedBox(height: 20),
+                  Text('Please enter your values'),
+
+                  SizedBox(height: 20),
+
+                  Container(
+                    // container containing the TextField
+                    width: 500,
+                    child: const TextField(
+                      // this text field need to update the productPrice state
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Product Cost Price',
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Container(
+                    // container containing the TextField
+                    width: 500,
+                    child: const TextField(
+                      // this textField need to update the amountPaid state
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter the amount paid with',
+                      ),
+                    keyboardType: TextInputType.number, // for numeric input only
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Button Clicked');
+                    },
+                    child: const Text('A Button'),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
